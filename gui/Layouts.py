10e55c099a77fb9b771d2d19c205467ecf196421
Przemysław Kleist <pk0202@protonmail.com>
2021-08-29 14:11:20 +0200

    second attempt to make grid layout

--- gui/Layouts.py
+++ gui/Layouts.py
@@ -328,60 +328,63 @@ class GridLayout(Layout):
         self.orientation = orientation
         self.x_start = x_start
         self.y_start = y_start
-        self.widgets = []
+        self.widgets = {}
         self.x_size, self.y_size = screen.get_size()
-        self.keep_padd_info = []
+        self.keep_padd_info = {}
         self.curr_x = 0
         self.curr_y = 0
-        self.curr_r = 0
-        self.curr_c = 0
 
     def add_widget(self, widget, row, col, xpadd=0, ypadd=0):
-        if row == 0 and col == 0:
-            self.widgets.append(["0", widget])
-        else:
-            self.widgets.append([self.v_or_h(row, col), widget])
-        self.keep_padd_info.append((xpadd, ypadd))
-        self.put(widget, row, col, xpadd, ypadd)
+        self.add_to_dict(self.widgets, row, widget)
+        self.add_to_dict(self.keep_padd_info, row, (xpadd, ypadd))
+        self.put(xpadd, ypadd)
 
     @ Layout.window_resize_callback
     def draw(self, screen_size, mouse_pos, mouse_button, keys, delta_time):
 
-        for widget in self.widgets:
-            widget[1].draw(self.screen, mouse_pos,
-                           mouse_button, keys, delta_time)
+        for _, widget_l in self.widgets.items():
+            for widget in widget_l:
+                widget.draw(self.screen, mouse_pos,
+                            mouse_button, keys, delta_time)
 
-    def put(self, widget, row, col, xpadd, ypadd):
-        for widget_l in self.widgets:
-            curr_w, curr_h = widget_l[1].get_size()
+    def put(self, xpadd, ypadd):
+        save_start_x = 0
+        for key, widget_l in self.widgets.items():
+            for i, widget in enumerate(widget_l):
+                curr_w, curr_h = widget.get_size()
+                if self.orientation == "C":
+                    if key == 0 and i == 0:
+                        self.curr_y = self.y_size/2 - curr_h/2 + self.y_start
+                        for key2, widget_l2 in self.widgets.items():
+                            self.curr_x = self.x_size/2 - curr_w/2 + self.x_start
+                            for j, curr_widget in enumerate(widget_l2):
+                                # FIXME: you need to find the longest row
+                                curr_w2, curr_h2 = curr_widget.get_size()
+                                if j != 0:
+                                    print(curr_w2)
+                                    self.curr_x -= curr_w2/2 - \
+                                        self.keep_padd_info[key2][j][0]/2
+                                else:
+                                    self.curr_x -= self.keep_padd_info[key2][j][0]/2
+                            if key2 != 0:
+                                self.curr_y -= curr_h2/2 - \
+                                    self.keep_padd_info[key2][j][1]/2
 
-            # bloated
-            if self.orientation == "C":
-                if widget_l[0] == "0":
-                    self.curr_x = self.x_size/2 - curr_w/2 + self.x_start
-                    self.curr_y = self.y_size/2 - curr_h/2 + self.y_start
+                        save_start_x = self.curr_x
+                    elif i == 0:
+                        self.curr_x = save_start_x
 
-                if widget_l[0] == "v":
-                    self.curr_y += curr_h  # + self.keep_padd_info[i]
-                elif widget_l[0] == "h":
-                    self.curr_x += curr_w  # + self.keep_padd_info[i]
-                widget.set_pos(self.curr_x, self.curr_y)
+                    widget.set_pos(self.curr_x, self.curr_y)
+                    self.curr_x += curr_w + self.keep_padd_info[key][i][0]
+            self.curr_y += curr_h + self.keep_padd_info[key][i][1]
 
-    def v_or_h(self, r, c):
-        rr = r - self.curr_r
-        cc = c - self.curr_c
-        print(rr, cc)
-
-        self.curr_r = r
-        self.curr_c = c
-        if rr >= 1 and cc <= 0:
-            return "h"
-        elif cc >= 1 and rr <= 0:
-            return "v"
-        else:
-            raise ValueError("YOU CAN'T PLACE IT LIKE THAT.")
+    def add_to_dict(self, dic, key, who):
+        if (row := dic.get(key)) is None:
+            dic[key] = row = []
+        row.append(who)
 
     def resize(self):
-        pass
-
+        for key, widget_l in self.widgets.items():
+            for i, widget in enumerate(widget_l):
+                self.put(*self.keep_padd_info[key][i])
 # --------------------------------------------------------------------- #
